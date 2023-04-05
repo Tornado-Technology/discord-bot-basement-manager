31fc0d7628729a88f821d2330b31081962f7cad0
Tornado-Technology <tornado.the.technologies@gmail.com>
2023-04-05 15:34:32 +1000

    Added required user for commands

--- src/commands/setProtection.ts
+++ src/commands/setProtection.ts
@@ -10,15 +10,21 @@ export default {
         .setName('state')
         .setDescription('State.')
         .setRequired(true)
-    )
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+    ),
   async execute(interaction: CommandInteraction) {
-    // @ts-ignore
-    client.protection = interaction.options.getString('state');
-    await interaction.reply({
-      content: `Server protection state set to ${client.protection ? 'enable' : 'disable'}`,
-      ephemeral: true,
-    });
+    if (interaction.user.id !== '967601899547295764' && interaction.user.id !== '344083304959705088') {
+      await interaction.reply({
+        content: 'You haven\'t permission for this action!',
+        ephemeral: true,
+      });
+    } else {
+      // @ts-ignore
+      client.protection = interaction.options.getString('state');
+      await interaction.reply({
+        content: `Server protection state set to ${client.protection ? 'enable' : 'disable'}`,
+        ephemeral: true,
+      });
+    }
     setTimeout(() => {
       interaction.deleteReply()
     }, 5000);
