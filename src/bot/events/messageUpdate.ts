5406d8261702c50c375a95a50e773762a665e69d
Tornado-Technology <tornado.the.technologies@gmail.com>
2023-04-16 21:29:41 +1000

    Fixed messageUpdate event

--- src/bot/events/messageUpdate.ts
+++ src/bot/events/messageUpdate.ts
@@ -4,8 +4,8 @@ export default {
   name: 'messageUpdate',
   async execute (oldMessage: Message, message: Message) {
     const member = message.member;
-    if (message.content.includes('🍍') && !member?.roles.cache.get('1088349504094359572')  && !member?.roles.cache.get('1048590352845635595')) {
-      await message.delete();
+    if (oldMessage?.content.includes('🍍') || message?.content.includes('🍍')  && !member?.roles.cache.get('1088349504094359572')  && !member?.roles.cache.get('1048590352845635595')) {
+      await oldMessage?.delete();
     }
   }
 };
