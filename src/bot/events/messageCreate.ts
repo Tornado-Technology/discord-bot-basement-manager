daf4977fc96dca893514dd4e7d2128ec1719acad
Tornado-Technology <tornado.the.technologies@gmail.com>
2023-04-20 19:02:38 +1000

    🍍🍍🍍

--- src/bot/events/messageCreate.ts
+++ src/bot/events/messageCreate.ts
@@ -4,7 +4,7 @@ export default {
   name: 'messageCreate',
   async execute (message: Message) {
     const member = message.member;
-    if (message.content.includes('🍍') && !member?.roles.cache.get('1088349504094359572')  && !member?.roles.cache.get('1048590352845635595') && member?.user?.id !== '828574119439368222' && member?.user?.id !== '260814562276671498') {
+    if (message.content.includes('🍍') && !member?.roles.cache.get('1088349504094359572')  && !member?.roles.cache.get('1048590352845635595') && member?.user.id !== '828574119439368222' && member?.user.id !== '260814562276671498') {
       await message.delete();
     }
   }
